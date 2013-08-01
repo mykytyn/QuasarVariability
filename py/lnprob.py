95b29834262db33ff1b09f264f11953c031042f4
mykytyn <dwm261@nyu.edu>
2013-08-01 15:53:13 +0000

    Covariance, and better grid plotting

--- py/lnprob.py
+++ py/lnprob.py
@@ -12,22 +12,33 @@ def ln_1d_gauss(x,m,sigmasqr):
     A = 1./np.sqrt(sigmasqr*2*np.pi)
     return np.log(A)-(1/2.)*(x-m)**2/(sigmasqr)
 
-def ln_like_array(g,r,sr2,sg2,a,b,s2,r_mean):
+def ln_like_array(tmag,gmag,g_mean,st2,sg2,a,b,s2):
     """
-    Inputs: g is the g magnitude
-            r is the r magnitude
-            r_mean is the r mean
-            sr2 is the variance of r
-            sg2 is the variance of g
+    Inputs: tmag is the target magnitude
+            gmag is the given magnitude
+            g_mean is the mean of the given magnitude
+            st2 is the variance of the target magnitude
+            sg2 is the variance of the given magnitude
             a is a scale factor (power)
             b is an offset
             s2 is a jitter in variance units
     Output: Log-likelihood
     """
-    return ln_1d_gauss(g,a*(r-r_mean)+b,a*a*sr2+sg2+s2)
+    return ln_1d_gauss(tmag,a*(gmag-g_mean)+b,a*a*sg2+st2+s2)
 
-def ln_prob(params,g,r,sg,sr):
-    a,b,s,r_mean = params
-    #g,r,sg,sg = data
+def ln_prob(params,t,g,st,sg):
+    """
+    Params:
+           a is a scale factor (power)
+           b is an offset
+           s is a jitter in std. dev units
+           g_mean is the mean of the given magnitude
+    Data:
+           t is the target magnitude
+           g is the given magnitude
+           st is the error of the target magnitude
+           sg is the error of the target magnitude
+    """
+    a,b,s,g_mean = params
 
-    return np.sum(ln_like_array(g,r,sr**2,sg**2,a,b,s**2,r_mean))
+    return np.sum(ln_like_array(t,g,g_mean,st**2,sg**2,a,b,s**2))
