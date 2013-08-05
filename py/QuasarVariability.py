aab2605ace8013725779f3cf743b3919daaaaea1
mykytyn <dwm261@nyu.edu>
2013-08-05 16:13:45 +0000

    This adds in multiband stuff (hopefully) also closes #4

--- py/QuasarVariability.py
+++ py/QuasarVariability.py
@@ -1,49 +1,58 @@
 import numpy as np
 
-class QuasarVariability:
-    def __init__(self, a, tau, mean):
-        """
-        inputs:
-        outputs:
-        comments:
-          - For now, a, tau, mean are all scalars.
-        """
-        # DM put in asserts that everything is scalar.
+class CovarianceFunction:
+    def __init__(self, a, tau):
+        assert len(a)==5
         self.a = a
         self.tau = tau
-        self.mean = mean
 
-    def get_mean_vector(self, times):
+    def _get_cross_term_matrix(self, t1, b1, t2, b2):
+        return np.matrix(self.a[b1[:,None]]*self.a[b2[None,:]] * np.exp(-1. / self.tau * np.abs(t1[:, None] - t2[None, :])))
+
+    def get_variance_tensor(self, times, bands, sigmas=None):
         """
         inputs:
         outputs:
         comments:
         """
-        return np.matrix(np.zeros_like(times) + self.mean).transpose()
-
-    def _get_cross_term_matrix(self, t1, t2):
-        return np.matrix(self.a ** 2 * np.exp(-1. / self.tau * np.abs(t1[:, None] -
-                                                                       t2[None, :])))
-
-    def get_variance_tensor(self, times, sigmas=None):
-        """
-        inputs:
-        outputs:
-        comments:
-        """
-        tt = self._get_cross_term_matrix(times, times)
+        tt = self._get_cross_term_matrix(times, bands, times, bands)
         if sigmas is None:
             return tt
         assert len(sigmas) == len(times)
         return tt + np.diag(sigmas ** 2)
+    
 
-    def get_prior_sample(self, times):
+class QuasarVariability:
+    def __init__(self, covar, mean):
+        """
+        inputs:
+        outputs:
+        comments:
+          - For now, a, tau, mean are all vectors.
+        """
+        assert len(mean)==5
+        self.covar = covar
+        self.mean = mean
+
+    def get_variance_tensor(self, times, bands, sigmas=None):
+        return self.covar.get_variance_tensor(times,bands,sigmas)
+
+    def get_mean_vector(self, times, bands):
         """
         inputs:
         outputs:
         comments:
         """
-        return np.random.multivariate_normal(np.array(self.get_mean_vector(times)).reshape(times.shape),self.get_variance_tensor(times),1)
+        return np.matrix([self.mean[x] for x in bands]).transpose()
+
+
+    def get_prior_sample(self, times,bands):
+        """
+        inputs:
+        outputs:
+        comments:
+        """
+        return np.random.multivariate_normal(np.array(self.get_mean_vector(times,bands)).reshape(times.shape),self.get_variance_tensor(times,bands),1)
 
     def _ln_multivariate_gaussian(self, x, mu, V):
         """
@@ -62,31 +71,32 @@ class QuasarVariability:
                 0.5 * dx.getT() * V.getI() * dx)
 
 
-    def ln_likelihood(self, mags, times, sigmas):
+    def ln_likelihood(self, mags, times, bands, sigmas):
         """
         inputs:
         outputs:
         comments:
         """
         return self._ln_multivariate_gaussian(np.matrix(mags).getT(),
-                                              self.get_mean_vector(times),
-                                              self.get_variance_tensor(times, sigmas))
+                                              self.get_mean_vector(times,bands),
+                                              self.get_variance_tensor(times, bands,sigmas))
 
-    def get_conditional_mean_and_variance(self, ptimes, mags, times, sigmas):
+    def get_conditional_mean_and_variance(self, ptimes, pbands, mags, times, bands, sigmas):
         """
         inputs:
         outputs:
         comments:
         """
-        Vpo = self._get_cross_term_matrix(ptimes, times)
-        Voo = self.get_variance_tensor(times, sigmas)
+        Vpo = self.covar._get_cross_term_matrix(ptimes, pbands, times, bands)
+        Voo = self.covar.get_variance_tensor(times, bands, sigmas)
         VooI = Voo.getI()
-        Vpp = self.get_variance_tensor(ptimes)
-        pmean = Vpo * VooI * (np.matrix(mags).getT() - self.get_mean_vector(times)) \
-            + self.get_mean_vector(ptimes)
+        Vpp = self.covar.get_variance_tensor(ptimes, pbands)
+        pmean = Vpo * VooI * (np.matrix(mags).getT() - self.get_mean_vector(times,bands)) \
+            + self.get_mean_vector(ptimes, pbands)
         Vpp = Vpp - Vpo * VooI * Vpo.getT()
         return pmean,Vpp
         
-    def get_conditional_sample(self, ptimes, mags, times, sigmas):
-        pmean,Vpp = self.get_conditional_mean_and_variance(ptimes,mags,times,sigmas)
+    def get_conditional_sample(self, ptimes, pbands, mags, times, bands, sigmas):
+        pmean,Vpp = self.get_conditional_mean_and_variance(ptimes, pbands, mags, times,
+                                                           bands, sigmas)
         return np.random.multivariate_normal(np.array(pmean).reshape(ptimes.shape), Vpp, 1)
--- py/testLikelihood.py
+++ py/testLikelihood.py
@@ -1,46 +1,131 @@
 import numpy as np
-from QuasarVariability import QuasarVariability
+from QuasarVariability import QuasarVariability,CovarianceFunction
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from mag_utils import meshgrid
+import pyfits
 
-a = .75
+bands_dict = {0:'u',1:'g',2:'r',3:'i',4:'z'}
+
+def make_prior_plots(testQ,timegrid,bands,pmean,psig,num_samps=8):
+    maggrids = []
+    plt.subplots_adjust(hspace=0,top=.95)
+    matplotlib.rc('xtick',labelsize=6)
+    matplotlib.rc('ytick',labelsize=6)
+    for i in range(num_samps):
+        maggrid = testQ.get_prior_sample(timegrid,bands)
+        maggrid = np.array(maggrid).reshape(timegrid.shape)
+        maggrids.append(maggrid)
+        
+    for i in range(5):
+        ax=plt.subplot(511+i)
+        mask = [bands==i]
+
+        btimegrid = timegrid[mask]
+        bpmean = pmean[mask]
+        bpsig = psig[mask]
+        plt.plot(btimegrid,bpmean,'k-')
+        plt.plot(btimegrid,bpmean-bpsig,'k-')
+        plt.plot(btimegrid,bpmean+bpsig,'k-')
+        for j in range(num_samps):
+            maggrid = maggrids[j]
+            plt.plot(btimegrid,maggrid[mask],'k-',alpha=0.25)
+        plt.setp(ax.get_xticklabels(),visible=(i==4))
+        plt.ylabel('%s' % bands_dict[i])
+
+def make_posterior_plots(testQ,test_times,test_mags,test_bands,test_sigmas,timegrid,bands,pmean,psig,num_samps=8):
+    maggrids = []
+    plt.subplots_adjust(hspace=0,top=.95)
+    matplotlib.rc('xtick',labelsize=8)
+    matplotlib.rc('ytick',labelsize=8)
+
+    for i in range(num_samps):
+        maggrid = testQ.get_conditional_sample(timegrid, bands, test_mags, test_times,
+                                               test_bands,test_sigmas)
+        maggrid = np.array(maggrid).reshape(timegrid.shape)
+        maggrids.append(maggrid)
+
+    for i in range(5):
+        ax=plt.subplot(511+i)
+        mask = [bands==i]
+        test_mask = [test_bands==i]
+        plt.ylabel('%s' % bands_dict[i])
+        plt.errorbar(test_times[test_mask],test_mags[test_mask],yerr=test_sigmas[test_mask],linestyle='none',color='black',marker='.')
+        btimegrid = timegrid[mask]
+        bpmean = pmean[mask]
+        bpsig = psig[mask]
+        plt.plot(btimegrid,bpmean,'k-')
+        plt.plot(btimegrid,bpmean-bpsig,'k-')
+        plt.plot(btimegrid,bpmean+bpsig,'k-')
+        for j in range(num_samps):
+            maggrid=maggrids[j]
+            plt.plot(btimegrid,maggrid[mask],'k-',alpha=0.25)
+        plt.setp(ax.get_xticklabels(),visible=(i==4))
+
+#a = np.array([.75,.85,.95,1.05,1.15])
 tau = 200.
-mean = 19.5
+#mean = np.array([19.5,20.5,21.5,22.5,23.5])
 
-testQ = QuasarVariability(a,tau,mean)
 
-dt = 3.
-timegrid = np.arange(0.5*dt,2000,dt)
+obj = 588015509825912905
+dt = 10.
+timegrid = np.arange(0.5*dt+51500,54500,dt)
+print len(timegrid)
+bands = np.zeros_like(timegrid)
+bands = np.array([np.random.randint(5) for x in bands])
+bandnames = ['u','g','r','i','z']
+means = []
+amps = []
+for name in bandnames:
+    mean, a = meshgrid(10., 30., 0., 1., 0.3, 0.008,name,obj)
+    means.append(mean)
+    amps.append(a)
+
+print means
+print amps
+means = np.array(means)
+amps = np.array(amps)
 plt.clf()
-pmean,Vpp=testQ.get_mean_vector(timegrid),testQ.get_variance_tensor(timegrid)
+testQ = QuasarVariability(CovarianceFunction(amps,tau),means)
+pmean=testQ.get_mean_vector(timegrid,bands)
+Vpp =testQ.get_variance_tensor(timegrid,bands)
 pmean=np.array(pmean).reshape(timegrid.shape)
 psig=np.sqrt(np.diag(np.array(Vpp)))
-plt.plot(timegrid,pmean,'k-')
-plt.plot(timegrid,pmean-psig,'k-')
-plt.plot(timegrid,pmean+psig,'k-')
-for i in range(8):
-    maggrid = testQ.get_prior_sample(timegrid)
-    maggrid = np.array(maggrid).reshape(timegrid.shape)
-    plt.plot(timegrid,maggrid,'k-',alpha=0.25)
+make_prior_plots(testQ,timegrid,bands,pmean,psig)
 plt.savefig("test_prior.png")
 
+hdulist = pyfits.open('quasar.fits')
+table = hdulist[1].data
+mask = table['headobjid']==obj
+table = table[mask]
 
-testTimes = np.array([500.,1500.,1700.])
-testMags = np.array([20.02,19.31,19.75])
-testSigmas = np.array([.05,.07,.05])
+testMags = []
+testSigmas = []
+testTimes = []
+testBands = []
 
+for i in range(5):
+    name = bands_dict[i]
+    mag = table['psfMag_%s' % name]
+    err = table['psfMagerr_%s' % name]
+    time = table['mjd_%s' % name]
+    band = [i]*len(mag)
+    testMags.extend(mag)
+    testSigmas.extend(err)
+    testBands.extend(band)
+    testTimes.extend(time)
+
+testMags = np.array(testMags)
+testSigmas = np.array(testSigmas)
+testBands = np.array(testBands)
+testTimes = np.array(testTimes)
 
 plt.clf()
-plt.errorbar(testTimes,testMags,yerr=testSigmas,linestyle='none',color='black',marker='.')
-pmean,Vpp=testQ.get_conditional_mean_and_variance(timegrid,testMags,testTimes,testSigmas)
+
+pmean,Vpp=testQ.get_conditional_mean_and_variance(timegrid, bands, testMags, testTimes,
+                                                  testBands, testSigmas)
 pmean=np.array(pmean).reshape(timegrid.shape)
 psig=np.sqrt(np.diag(np.array(Vpp)))
-plt.plot(timegrid,pmean,'k-')
-plt.plot(timegrid,pmean-psig,'k-')
-plt.plot(timegrid,pmean+psig,'k-')
-for i in range(8):
-    maggrid = testQ.get_conditional_sample(timegrid,testMags,testTimes,testSigmas)
-    maggrid = np.array(maggrid).reshape(timegrid.shape)
-    plt.plot(timegrid,maggrid,'k-',alpha=0.25)
+make_posterior_plots(testQ,testTimes,testMags,testBands,testSigmas,timegrid,bands,pmean,psig)
 plt.savefig("test_posterior.png")
