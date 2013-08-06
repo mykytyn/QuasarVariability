d4eea315705acdb006f7fa11847ddbfe4f697672
mykytyn <dwm261@nyu.edu>
2013-08-06 12:17:42 +0000

    add in docs strings, make mostly pep8 etc. fixes #9

--- py/QuasarVariability.py
+++ py/QuasarVariability.py
@@ -1,58 +1,74 @@
 import numpy as np
 
+
 class CovarianceFunction:
     def __init__(self, a, tau):
-        assert len(a)==5
+        """
+        Inputs: a: five-vector of amplitudes in each band
+                tau: scalar of time scale (in days)
+        Outputs: None
+        Comments: Damped random walk model
+        """
+
+        assert len(a) == 5
         self.a = a
         self.tau = tau
 
     def _get_cross_term_matrix(self, t1, b1, t2, b2):
-        return np.matrix(self.a[b1[:,None]]*self.a[b2[None,:]] * np.exp(-1. / self.tau * np.abs(t1[:, None] - t2[None, :])))
+        return np.matrix(self.a[b1[:, None]] * self.a[b2[None, :]] *
+                         np.exp(-1. / self.tau * np.abs(t1[:, None] - t2[None, :])))
 
     def get_variance_tensor(self, times, bands, sigmas=None):
         """
-        inputs:
-        outputs:
-        comments:
+        Inputs: times: an array of times (in days)
+                bands: a vector associating each point with a band (using 0=u, etc.)
+                sigmas: an array of variances
+        Outputs:
+                the variance tensor
+        Comments:
         """
         tt = self._get_cross_term_matrix(times, bands, times, bands)
         if sigmas is None:
             return tt
         assert len(sigmas) == len(times)
         return tt + np.diag(sigmas ** 2)
-    
+
 
 class QuasarVariability:
     def __init__(self, covar, mean):
         """
-        inputs:
-        outputs:
-        comments:
-          - For now, a, tau, mean are all vectors.
+        Inputs: covar: a covariance function object
+                mean: a five-vector of the mean in each band
+        Outputs:
+        Comments:
         """
-        assert len(mean)==5
+        assert len(mean) == 5
         self.covar = covar
         self.mean = mean
 
     def get_variance_tensor(self, times, bands, sigmas=None):
-        return self.covar.get_variance_tensor(times,bands,sigmas)
+        return self.covar.get_variance_tensor(times, bands, sigmas)
 
     def get_mean_vector(self, times, bands):
         """
-        inputs:
-        outputs:
-        comments:
+        Inputs: times: an array of times (in days)
+                bands: a vector associating each point with a band (using 0=u, etc.)
+                sigmas: an array of variances
+        Outputs:
+                the variance tensor
+        Comments:
         """
         return np.matrix([self.mean[x] for x in bands]).transpose()
 
-
-    def get_prior_sample(self, times,bands):
+    def get_prior_sample(self, times, bands):
         """
-        inputs:
-        outputs:
-        comments:
+        Inputs: times: an array of times we want to sample at
+                bands: an array of bands associated with each time
+        Outputs:
+                a draw from the prior
+        Comments:
         """
-        return np.random.multivariate_normal(np.array(self.get_mean_vector(times,bands)).reshape(times.shape),self.get_variance_tensor(times,bands),1)
+        return np.random.multivariate_normal(np.array(self.get_mean_vector(times, bands)).reshape(times.shape), self.get_variance_tensor(times, bands), 1)
 
     def _ln_multivariate_gaussian(self, x, mu, V):
         """
@@ -64,39 +80,58 @@ class QuasarVariability:
         NOTE: there must be a way to compute determinant/inverse faster
         """
 
-        A=1/np.sqrt((2*np.pi)**len(x))
-        assert len(x)==len(mu)==V.shape[0]==V.shape[1]
-        dx = x-mu
-        return (np.log(A) - 0.5 * np.linalg.slogdet(V)[1] - 
+        A = 1 / np.sqrt((2 * np.pi) ** len(x))
+        assert len(x) == len(mu) == V.shape[0] == V.shape[1]
+        dx = x - mu
+        return (np.log(A) - 0.5 * np.linalg.slogdet(V)[1] -
                 0.5 * dx.getT() * V.getI() * dx)
-
 
     def ln_likelihood(self, mags, times, bands, sigmas):
         """
-        inputs:
-        outputs:
-        comments:
+        Inputs: mags: an array of mags
+                times: an array of times
+                bands: an array of bands associated with each time
+                sigmas: the variances
+        Outputs:
+                The ln of the likelihood function
+        Comments:
         """
         return self._ln_multivariate_gaussian(np.matrix(mags).getT(),
-                                              self.get_mean_vector(times,bands),
-                                              self.get_variance_tensor(times, bands,sigmas))
+                                              self.get_mean_vector(times, bands),
+                                              self.get_variance_tensor(times, bands, sigmas))
 
     def get_conditional_mean_and_variance(self, ptimes, pbands, mags, times, bands, sigmas):
         """
-        inputs:
-        outputs:
-        comments:
+        Inputs: ptimes: array of times to test at
+                pbands: array of bands matching the ptimes
+                mags: measured mags
+                times: measured times
+                bands: measured bands
+                sigmas: measured variances
+        Outputs: pmean: mean at ptimes
+                 Vpp: covariance matrix
+        Comments:
         """
         Vpo = self.covar._get_cross_term_matrix(ptimes, pbands, times, bands)
         Voo = self.covar.get_variance_tensor(times, bands, sigmas)
         VooI = Voo.getI()
         Vpp = self.covar.get_variance_tensor(ptimes, pbands)
-        pmean = Vpo * VooI * (np.matrix(mags).getT() - self.get_mean_vector(times,bands)) \
+        pmean = Vpo * VooI * (np.matrix(mags).getT() - self.get_mean_vector(times, bands)) \
             + self.get_mean_vector(ptimes, pbands)
         Vpp = Vpp - Vpo * VooI * Vpo.getT()
-        return pmean,Vpp
-        
+        return pmean, Vpp
+
     def get_conditional_sample(self, ptimes, pbands, mags, times, bands, sigmas):
-        pmean,Vpp = self.get_conditional_mean_and_variance(ptimes, pbands, mags, times,
-                                                           bands, sigmas)
+        """
+        Inputs: ptimes: array of times to sample at
+                pbands: array of bands matching the ptimes
+                mags: measured mags
+                times: measured times
+                bands: measured bands
+                sigmas: measured variances
+        Outputs: A draw from the multivariate gaussian at ptimes
+        Comments:
+        """
+        pmean, Vpp = self.get_conditional_mean_and_variance(ptimes, pbands, mags, times,
+                                                            bands, sigmas)
         return np.random.multivariate_normal(np.array(pmean).reshape(ptimes.shape), Vpp, 1)
