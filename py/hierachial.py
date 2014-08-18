2fee9d205e8be39a056f2787b10234c0940f7ca5
mykytyn <dwm261@nyu.edu>
2014-08-18 13:22:46 +0000

    hierachy skeleton

--- py/hierachial.py
+++ py/hierachial.py
@@ -1,7 +1,7 @@
-
 import cPickle
 import numpy as np
 from scipy.optimize import leastsq
+import scipy
 import matplotlib
 matplotlib.use('Agg')
 import utils
@@ -9,8 +9,61 @@ import utils
 
 def main():
     f = open('newtargetlist.txt', 'r')
-    first = int(f.readline())
-    print first
+    targets = [int(x) for x in f]
+    print lnprob(targets, 300., 50.)
+    f.close()
+
+
+def importance_sample_one(objid, tau_mean, tau_variance):
+    """
+    takes in one id
+    and also mean and sigma
+    gives importance samp
+    """
+
+    g = open("{}.pickle".format(objid))
+    quasar, quasar_data, flatchain, lnprobability, labels = cPickle.load(g)
+    taus = np.exp(flatchain[:, 7])
+    g.close()
+    return np.log(np.sum(np.exp(utils.ln_1d_gauss(taus,
+                                                  tau_mean, tau_variance))))
+
+
+def importance_sample_all(targlist, tau_mean, tau_variance):
+    """
+    takes an id list
+    and also mean and sigma
+    returns total ln prob
+    """
+
+    total = 0
+    for targ in targlist:
+        total += importance_sample_one(targ, tau_mean, tau_variance)
+    return total
+
+
+def ln_prior(tau_mean, tau_variance):
+    """
+    this is only temp
+    waiting for more information
+    should cut small sigma
+    """
+
+    return 0
+
+
+def ln_prob(targets, tau_mean, tau_variance):
+    """
+    this takes a targ list
+    and hyperparameters
+    and gives the ln prob
+    """
+
+    prior = ln_prior(tau_mean, tau_variance)
+    if np.isinf(prior):
+        return -np.inf
+    return prior + importance_sample_all(targets, tau_mean, tau_variance)
+
 
 def make_large_triangle_plot():
     """
@@ -22,7 +75,7 @@ def make_large_triangle_plot():
     f = open('newtargetlist.txt', 'r')
     hugeflatchains = []
     hugelnprobabilities = []
-    for numobj,obj in enumerate(f):
+    for numobj, obj in enumerate(f):
         prefix = obj
         obj = int(obj)
         g = open("{}.pickle".format(obj))
@@ -35,7 +88,8 @@ def make_large_triangle_plot():
     print hugeflatchain.shape
     hugelnprobability = np.hstack(hugelnprobabilities)
     print hugelnprobability.shape
-    utils.make_triangle_plot(hugelnprobability, hugeflatchain, labels).savefig('hugetriangle.png')
+    utils.make_triangle_plot(hugelnprobability,
+                             hugeflatchain, labels).savefig('hugetriangle.png')
 
 if __name__ == '__main__':
     main()
