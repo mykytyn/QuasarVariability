1f3ef050546334f6bb0f9899b0eb32c148aa9d48
mykytyn <dwm261@nyu.edu>
2016-06-10 19:48:31 +0000

    triangle -> corner

--- py/utils.py
+++ py/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyfits as pyf
 import matplotlib
 import matplotlib.pyplot as plt
-import triangle
+import corner
 
 
 
@@ -17,7 +17,8 @@ def make_triangle_plot(lnprobability, flatchain, labels, temp=False):
         extents = list([[np.min(x[np.isfinite(x)]), np.max(x[np.isfinite(x)])] for
                         x in np.hsplit(trichain, trichain.shape[1])])
         extents.append([np.min(lnprob[np.isfinite(lnprob)]), np.max(lnprob)])
-        return triangle.corner(trichain, labels=labels, extents=extents) #TEMP
+        print extents
+        return corner.corner(trichain, labels=labels, extents=extents) #TEMP
 
     lnprob = lnprobability.flatten()
     trichain = np.column_stack((flatchain, lnprob))
@@ -25,7 +26,7 @@ def make_triangle_plot(lnprobability, flatchain, labels, temp=False):
     extents = list([[np.min(x[np.isfinite(x)]), np.max(x[np.isfinite(x)])] for
                     x in np.hsplit(trichain, trichain.shape[1])])
     extents.append([np.min(lnprob[np.isfinite(lnprob)]), np.max(lnprob)])
-    return triangle.corner(trichain[:,5:], labels=labels[5:], extents=extents[5:])#TEMP
+    return corner.corner(trichain[:,5:], labels=labels[5:], extents=extents[5:])#TEMP
 
 
 def get_best_lnprob(sampler):
